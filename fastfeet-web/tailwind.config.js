de306eef83bbc69d33ae15c1a58c12c7b3cd8dee
Hyarlei Silva <hyarleysf@gmail.com>
2026-01-07 09:13:47 -0300

    feat: Iniciando fastfeet

---
+++ fastfeet-web/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          purple: '#7D40E7',
+          gray: '#999999',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
